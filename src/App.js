dcc387ee4e1b03684cbe946b09f721793ad2ad6d
Hossein Keshtkar Haranaki <rock.hkh.g1@gmail.com>
2021-10-03 23:32:07 +0330

    update

--- src/App.js
+++ src/App.js
@@ -26,6 +26,24 @@ class App extends Component {
     return (
       <>
         <Header filters={STATE.news} onSearch={this.searchHandler} />
+        <p
+          style={{
+            padding: "25px",
+            fontFamily: "Helvetica Neue",
+            fontSize: "1.2rem",
+            backgroundColor: "green",
+            borderRadius: "10px",
+            maxWidth: "900px",
+            color: "#fff",
+            margin: "10px auto",
+          }}
+        >
+          So, we have some dummy news here that we can filter them specifically
+          by searching keywords in the input bar. In order to see the
+          functionality, search a keyword from the title of any news. Just keep
+          in mind that capital letters matter here. This function is a really
+          really simple one; it's not a perfect searching engine.
+        </p>
         <NewsList news={STATE.filtered} />
         {/* <Footer footer={STATE.footer} /> */}
       </>
